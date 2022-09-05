9e824d9b7c319ad2f5b462d726320f357bd33c0b
Ryan Su <yuansu.china.work@gmail.com>
2022-09-05 10:46:43 +0800

    fix: api bug

--- src/api/file/initialize.ts
+++ src/api/file/initialize.ts
@@ -2,7 +2,7 @@ import { defHttp } from '/@/utils/http/axios';
 import { BaseResp } from '/@/api/model/baseModel';
 
 enum Api {
-  InitializeDatabase = '/file/init/database',
+  InitializeDatabase = '/file-manager/init/database',
 }
 
 /**
