dd88ea5511423faa5b72702cc5299702c2d9a1a6
Ryan SU <1137661202@qq.com>
2022-11-12 09:59:59 +0800

    fix: navigation translation

---
+++ src/locales/lang/en/routes/system.ts
@@ -0,0 +1,14 @@
+export default {
+  systemManagementTitle: 'System',
+  menuManagementTitle: 'Menu',
+  roleManagementTitle: 'Role',
+  apiManagementTitle: 'API',
+  userManagementTitle: 'User',
+  fileManagementTitle: 'File',
+  userProfileTitle: 'Profile',
+  dictionaryManagementTitle: 'Dictionary',
+  dictionaryDetailManagementTitle: 'Key/Value Management',
+  oauthManagement: 'Oauth',
+  tokenManagement: 'Token',
+  otherPages: 'Other pages',
+};
---
+++ src/locales/lang/zh-CN/routes/system.ts
@@ -0,0 +1,14 @@
+export default {
+  systemManagementTitle: '系统管理',
+  menuManagementTitle: '菜单管理',
+  roleManagementTitle: '角色管理',
+  apiManagementTitle: 'API管理',
+  userManagementTitle: '用户管理',
+  fileManagementTitle: '文件管理',
+  userProfileTitle: '用户个人信息',
+  dictionaryManagementTitle: '字典管理',
+  dictionaryDetailManagementTitle: '键值管理',
+  oauthManagement: 'Oauth管理',
+  tokenManagement: 'Token管理',
+  otherPages: '其他页面',
+};
