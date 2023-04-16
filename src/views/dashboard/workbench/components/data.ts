300e08dc4284a216dac5cc549e22366614b6cad9
Ryan Su <yuansu.china.work@gmail.com>
2023-04-17 00:17:16 +0800

    chore: update system version

--- src/views/dashboard/workbench/components/data.ts
+++ src/views/dashboard/workbench/components/data.ts
@@ -52,5 +52,5 @@ export const navItems: NavItem[] = [
 
 export const systemInfoData = [
   ['sys.sys.Name', 'Simple Admin'],
-  ['sys.sys.version', 'V 0.3.0'],
+  ['sys.sys.version', 'V 1.0.0'],
 ];
