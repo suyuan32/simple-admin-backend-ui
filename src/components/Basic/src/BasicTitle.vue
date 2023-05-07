9fae05fe6bf96faeffc528625a2f173a26759f5e
Ryan SU <yuansu.china.work@gmail.com>
2023-05-07 11:05:35 +0800

    fix: drawer component dark mode bugs

--- src/components/Basic/src/BasicTitle.vue
+++ src/components/Basic/src/BasicTitle.vue
@@ -42,6 +42,40 @@
 <style lang="less" scoped>
   @prefix-cls: ~'@{namespace}-basic-title';
 
+  [data-theme='dark'] {
+    .@{prefix-cls} {
+      display: flex;
+      position: relative;
+      padding-left: 7px;
+      color: white;
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 24px;
+      cursor: pointer;
+      user-select: none;
+
+      &-normal {
+        font-size: 14px;
+        font-weight: 500;
+      }
+
+      &-show-span::before {
+        content: '';
+        position: absolute;
+        top: 4px;
+        left: 0;
+        width: 3px;
+        height: 16px;
+        margin-right: 4px;
+        background-color: @primary-color;
+      }
+
+      &-help {
+        margin-left: 10px;
+      }
+    }
+  }
+
   .@{prefix-cls} {
     display: flex;
     position: relative;
--- src/components/Drawer/src/components/DrawerFooter.vue
+++ src/components/Drawer/src/components/DrawerFooter.vue
@@ -66,7 +66,25 @@
 
 <style lang="less">
   @prefix-cls: ~'@{namespace}-basic-drawer-footer';
+
   @footer-height: 60px;
+
+  [data-theme='dark'] {
+    .@{prefix-cls} {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 0 12px 0 20px;
+      border-top: 1px solid gray;
+      background-color: #262626;
+      text-align: right;
+
+      > * {
+        margin-right: 8px;
+      }
+    }
+  }
+
   .@{prefix-cls} {
     position: absolute;
     bottom: 0;
