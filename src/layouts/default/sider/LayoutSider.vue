a1e8abee356d1ced018d5f22887bf30778b74502
Ryan SU <yuansu.china.work@gmail.com>
2023-10-26 07:02:25 +0000

    fix: sider color

--- src/layouts/default/sider/LayoutSider.vue
+++ src/layouts/default/sider/LayoutSider.vue
@@ -154,4 +154,55 @@
       line-height: 36px;
     }
   }
+
+  [data-theme='dark'] {
+    .@{prefix-cls} {
+      z-index: @layout-sider-fixed-z-index;
+
+      &--fixed {
+        position: fixed !important;
+        top: 0;
+        left: 0;
+        height: 100%;
+      }
+
+      &--mix {
+        top: @header-height;
+        height: calc(100% - @header-height);
+      }
+
+      &.dark-layout-sider {
+        background-color: @sider-dark-bg-color;
+
+        .dark-layout-sider-trigger {
+          background-color: @trigger-dark-bg-color;
+          color: darken(@white, 25%);
+
+          &:hover {
+            background-color: @trigger-dark-hover-bg-color;
+            color: @white;
+          }
+        }
+      }
+
+      &:not(.dark-layout-sider) {
+        // box-shadow: 2px 0 8px 0 rgba(29, 35, 41, 0.05);
+
+        .dark-layout-sider-trigger {
+          border-top: 1px solid @border-color-light;
+          color: @text-color-base;
+        }
+      }
+
+      .dark-layout-sider-zero-width-trigger {
+        z-index: 10;
+        top: 40%;
+      }
+
+      & .dark-layout-sider-trigger {
+        height: 36px;
+        line-height: 36px;
+      }
+    }
+  }
 </style>
