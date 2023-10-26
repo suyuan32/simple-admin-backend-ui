98578324f067e472174b17730bb60830c331cb9d
Ryan SU <yuansu.china.work@gmail.com>
2023-10-26 02:33:18 +0000

    refactor: use unplugin-config

--- internal/vite-config/src/plugins/appConfig.ts
+++ internal/vite-config/src/plugins/appConfig.ts
@@ -1,96 +1,31 @@
-import colors from 'picocolors';
-import { readPackageJSON } from 'pkg-types';
+import GenerateConfig from 'unplugin-config/vite';
 import { type PluginOption } from 'vite';
 
 import { getEnvConfig } from '../utils/env';
-import { createContentHash } from '../utils/hash';
+import { strToHex } from '../utils/hash';
 
 const GLOBAL_CONFIG_FILE_NAME = '_app.config.js';
-const PLUGIN_NAME = 'app-config';
-
-async function createAppConfigPlugin({
-  root,
-  isBuild,
-}: {
-  root: string;
-  isBuild: boolean;
-}): Promise<PluginOption> {
-  let publicPath: string;
-  let source: string;
-  if (!isBuild) {
-    return {
-      name: PLUGIN_NAME,
-    };
-  }
-  const { version = '' } = await readPackageJSON(root);
-
-  return {
-    name: PLUGIN_NAME,
-    async configResolved(_config) {
-      let appTitle = _config?.env?.VITE_GLOB_APP_TITLE ?? '';
-      appTitle = appTitle.replace(/\s/g, '_');
-      publicPath = _config.base;
-      source = await getConfigSource(appTitle);
-    },
-    async transformIndexHtml(html) {
-      publicPath = publicPath.endsWith('/') ? publicPath : `${publicPath}/`;
-
-      const appConfigSrc = `${
-        publicPath || '/'
-      }${GLOBAL_CONFIG_FILE_NAME}?v=${version}-${createContentHash(source)}}`;
-
-      return {
-        html,
-        tags: [
-          {
-            tag: 'script',
-            attrs: {
-              src: appConfigSrc,
-            },
-          },
-        ],
-      };
-    },
-    async generateBundle() {
-      try {
-        this.emitFile({
-          type: 'asset',
-          fileName: GLOBAL_CONFIG_FILE_NAME,
-          source,
-        });
-
-        console.log(colors.cyan(`✨configuration file is build successfully!`));
-      } catch (error) {
-        console.log(
-          colors.red('configuration file configuration file failed to package:\n' + error),
-        );
-      }
-    },
-  };
-}
-
-/**
- * Get the configuration file variable name
- * @param env
- */
-const getVariableName = (title: string) => {
-  return `__PRODUCTION__${title || '__APP'}__CONF__`.toUpperCase().replace(/\s/g, '');
-};
-
-async function getConfigSource(appTitle: string) {
+// This constant sets the output directory for the Vite package
+const OUTPUT_DIR = 'dist';
+export async function createConfigPluginConfig(
+  shouldGenerateConfig: boolean,
+): Promise<PluginOption> {
   const config = await getEnvConfig();
-  const variableName = getVariableName(appTitle);
-  const windowVariable = `window.${variableName}`;
-  // Ensure that the variable will not be modified
-  let source = `${windowVariable}=${JSON.stringify(config)};`;
-  source += `
-    Object.freeze(${windowVariable});
-    Object.defineProperty(window, "${variableName}", {
-      configurable: false,
-      writable: false,
-    });
-  `.replace(/\s/g, '');
-  return source;
+  // LINK /src/utils/env.ts -> getVariableName
+  const APP_NAME = strToHex(config?.VITE_GLOB_APP_TITLE ?? '__APP');
+  // https://github.com/kirklin/unplugin-config
+  return GenerateConfig({
+    appName: APP_NAME,
+    envVariables: {
+      prefix: 'VITE_GLOB_',
+    },
+    configFile: {
+      generate: shouldGenerateConfig,
+      fileName: GLOBAL_CONFIG_FILE_NAME,
+      outputDir: OUTPUT_DIR,
+    },
+    htmlInjection: {
+      decodeEntities: true,
+    },
+  });
 }
-
-export { createAppConfigPlugin };
--- internal/vite-config/src/plugins/index.ts
+++ internal/vite-config/src/plugins/index.ts
@@ -1,11 +1,9 @@
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-// @ts-ignore: type unless
-import DefineOptions from 'unplugin-vue-define-options/vite';
 import { type PluginOption } from 'vite';
 import purgeIcons from 'vite-plugin-purge-icons';
 
-import { createAppConfigPlugin } from './appConfig';
+import { createConfigPluginConfig } from './appConfig';
 import { configCompressPlugin } from './compress';
 import { configHtmlPlugin } from './html';
 import { configMockPlugin } from './mock';
@@ -20,10 +18,10 @@ interface Options {
   enableAnalyze?: boolean;
 }
 
-async function createPlugins({ isBuild, root, enableMock, compress, enableAnalyze }: Options) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx(), DefineOptions()];
+async function createPlugins({ isBuild, enableMock, compress, enableAnalyze }: Options) {
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()];
 
-  const appConfigPlugin = await createAppConfigPlugin({ root, isBuild });
+  const appConfigPlugin = await createConfigPluginConfig(isBuild);
   vitePlugins.push(appConfigPlugin);
 
   // vite-plugin-html
