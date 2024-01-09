45f8e29e0c42ce14525a651c13d929710b5a05e2
nullcache <nullcache@outlook.com>
2024-01-09 18:59:04 +0800

    fix: add dictionary cache and remove vue-request

--- src/components/Table/src/components/DictionaryTag.vue
+++ src/components/Table/src/components/DictionaryTag.vue
@@ -8,8 +8,6 @@
   import { propTypes } from '/@/utils/propTypes';
   import { useDictionaryStore } from '/@/store/modules/dictionary';
   import { DefaultOptionType } from 'ant-design-vue/lib/select';
-  import { GetDictionaryDetailByDictionaryName } from '/@/api/sys/dictionaryDetail';
-  import { useRequest } from 'vue-request';
 
   export default defineComponent({
     name: 'DictionaryTag',
@@ -29,40 +27,10 @@
       });
 
       async function handleFetch() {
-        if (props.cache) {
-          const dictStore = useDictionaryStore();
-          const dictData = await dictStore.getDictionary(props.dictionaryName);
-          if (dictData != null) {
-            options.value = dictData.data;
-          }
-        } else {
-          useRequest(
-            async () => {
-              return GetDictionaryDetailByDictionaryName({
-                name: props.dictionaryName,
-              });
-            },
-            {
-              cacheKey: '1',
-              cacheTime: 1000, // 1 second
-              onSuccess: (data) => {
-                console.log(1);
-                const result = data;
-                if (result.code === 0) {
-                  const dataConv = ref<DefaultOptionType[]>([]);
-                  for (let i = 0; i < result.data.total; i++) {
-                    dataConv.value.push({
-                      label: result.data.data[i].title,
-                      value: result.data.data[i].value,
-                    });
-                  }
-                  options.value = dataConv.value;
-                } else {
-                  options.value = undefined;
-                }
-              },
-            },
-          );
+        const dictStore = useDictionaryStore();
+        const dictData = await dictStore.getDictionary(props.dictionaryName, props.cache);
+        if (dictData != null) {
+          options.value = dictData.data;
         }
       }
 
