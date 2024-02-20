c2277348275097b240ce5bd054a4e1632a10d9a5
Ryan Su <yuansu.china.work@gmail.com>
2024-02-20 12:23:50 +0800

    feat(deps): update vite version to 5.x

--- src/locales/lang/en.ts
+++ src/locales/lang/en.ts
@@ -1,10 +1,10 @@
 import { genMessage } from '../helper';
 import antdLocale from 'ant-design-vue/es/locale/en_US';
 
-const modules = import.meta.globEager('./en/**/*.ts');
+const modules = import.meta.glob('./en/**/*.{json,ts,js}', { eager: true });
 export default {
   message: {
-    ...genMessage(modules as Record<string, Record<string, any>>, 'en'),
+    ...genMessage(modules as Recordable<Recordable>, 'en'),
     antdLocale,
   },
   dateLocale: null,
