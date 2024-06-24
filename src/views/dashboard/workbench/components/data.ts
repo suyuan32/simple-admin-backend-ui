6f1d686cec105b571a91ebe39f8f686cb966724e
Ryan Su <yuansu.china.work@gmail.com>
2024-06-24 22:33:43 +0800

    chore: update deps

--- src/views/dashboard/workbench/components/data.ts
+++ src/views/dashboard/workbench/components/data.ts
@@ -52,5 +52,5 @@ export const navItems: NavItem[] = [
 
 export const systemInfoData = [
   ['sys.sys.Name', 'Simple Admin'],
-  ['sys.sys.version', 'V 1.4.6'],
+  ['sys.sys.version', 'V 1.4.8'],
 ];
