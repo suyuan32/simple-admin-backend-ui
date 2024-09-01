ec8fe05f67c4ac06b7c05150665de72afa847355
Ryan Su <yuansu.china.work@gmail.com>
2024-09-01 11:38:38 +0800

    refactor: remove vxe table supports

--- src/components/VxeTable/src/components/ACheckboxGroup.tsx
+++
@@ -1,6 +0,0 @@
-import { VxeGlobalRendererOptions } from 'vxe-table';
-import { createFormItemRender } from './common';
-
-export default {
-  renderFormItemContent: createFormItemRender(),
-} as VxeGlobalRendererOptions;
